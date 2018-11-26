04309f30fc0f7db12fb97c74fa74fb8f95bfd119
Felixandra27 <45374682+Felixandra27@users.noreply.github.com>
2018-11-27 07:40:13 +0800

    Create  sample_andal.py

---
+++ sample_andal.py
@@ -0,0 +1,38 @@
+# This is a Sample test of Python
+
+#Strings
+
+car = " honda civic "
+phone = " iphone "
+laptop = "ProMACbook"
+
+print(car)
+print(phone)
+print(laptop)
+
+#Numerical value
+
+class_number_at_room = 35
+age = 18
+time_schedules = 7
+
+print(class_number_at_room)
+print(age)
+print(time_schedules)
+
+#Arithmetic
+
+division = 10 / 100
+modules = 100 % 4
+subtraction = 100 - 10
+
+print(division)
+print(modules)
+print(subtraction)
+
+if division > subtraction:
+    print("This is a basic math")
+elif modules > subtraction:
+    print("This is the answer")
+else:
+    print("This is invalid")
