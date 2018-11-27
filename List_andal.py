bb97c9b75eb55517be6e1a73045d8ad745b054ba
Felixandra27 <45374682+Felixandra27@users.noreply.github.com>
2018-11-27 09:54:12 +0800

    Create List_andal.py

---
+++ List_andal.py
@@ -0,0 +1,18 @@
+#SAMPLE OF ARRAY
+
+mga_dogs = ["arvi","km","andal"]
+mga_numbers = [1,2,3,4,5,6]
+mga_decimal_points = [1.0,1.2,1.3,1.4]
+
+for x in range(len(mga_dogs)):
+     print(mga_dogs[x])
+
+for y in range(len(mga_numbers)):
+    print(mga_numbers[y])
+
+for z in range(len(mga_decimal_points)):
+    print(mga_decimal_points[z])
+
+print (len(mga_dogs))
+print (len(mga_numbers))
+print (len(mga_decimal_points))
