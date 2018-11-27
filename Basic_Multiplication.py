ff4c7219ff9addac1d49c3c32156ec40d82cb9d6
Felixandra27 <45374682+Felixandra27@users.noreply.github.com>
2018-11-27 09:57:38 +0800

    Create Basic_Multiplication.py

---
+++ Basic_Multiplication.py
@@ -0,0 +1,15 @@
+#BASIC MULTIPLICATION
+#EVEN AND ODD NUMBERS TO 0 AND E
+for x in range(1, 11):
+    for y in range(1, 11):
+       z = x * y
+
+       if z % 2 == 0:
+           print ("e", end = '\t')
+       if z % 1 == 0:
+           print ("o", end = '\t')
+       else:
+           print (x*y, end='\t')
+
+
+    print()
