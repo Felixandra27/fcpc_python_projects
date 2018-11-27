f7f6789ef37632e998c1b0fcad40b60fe4169276
Felixandra27 <45374682+Felixandra27@users.noreply.github.com>
2018-11-27 11:16:59 +0800

    Create activity_b_andal.py

---
+++ activity_b_andal.py
@@ -0,0 +1,18 @@
+#ACTIVITY B
+#01.
+for x in range(8):
+    print(2**x, end = '\t')
+print()
+#02.
+for x in range(1,13):
+    if x % 4 == 0:
+      print("s", end = '\t')
+    else:
+      print (x, end='\t')
+print()
+#03.
+for f in range (1,9):
+    if f % 8 == 1:
+        print(f, end = '\t')
+    else:
+        print("E", end='\t')
