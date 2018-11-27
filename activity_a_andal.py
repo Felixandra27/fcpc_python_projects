75fc126509f162152d17e0f9ae3d7563203cc185
Felixandra27 <45374682+Felixandra27@users.noreply.github.com>
2018-11-27 11:17:30 +0800

    Create activity_a_andal.py

---
+++ activity_a_andal.py
@@ -0,0 +1,15 @@
+#ACTIVITY A
+#01.
+for x in range(1,17,3):
+    print(x, end = '\t')
+print()
+#02.
+for y in range(100,75,-7):
+    print(y, end = '\t')
+print()
+#03.
+for f in range (1,5):
+    if f % 3 == 0:
+        print(1, end = '\t')
+    else:
+        print(0, end='\t')
