e00417bc88b24d0d6f3a6c7f5078a64876788e60
Felixandra27 <45374682+Felixandra27@users.noreply.github.com>
2018-12-11 08:56:14 +0800

    Add files via upload

---
+++ felix.py
@@ -0,0 +1,40 @@
+print("FELIXANDRA F. ANDAL")
+print("BSIT 1-A")
+print("")
+#THIS IS AN EXAMPLE OF LOOPS PYTHON
+#ACTIVY A
+print("I.APPLY THE FF. IN YOUR CODE:")
+print("")
+print("--------------------------")
+
+print("1. VARIABLES")
+
+val1 = 1.75
+val2 = 1.25
+val3 = 1.75
+
+val4 = val1 + val2 + val3 / 3
+print(val4)
+print("--------------------------")
+print("2.OPERATORS")
+
+val1 = 30
+val2 = 50
+val3 = 50
+
+val4 = val2 % val3 >= val1
+print(val4)
+
+print("--------------------------")
+print("3. CONDITIONALS")
+
+val1 = 27
+if val1 >=10:
+    print("FELIX")
+print("HELLO PAPS")
+
+print("--------------------------")
+print("4.LOOPS")
+
+for x in range(1,6):
+    print("x")
