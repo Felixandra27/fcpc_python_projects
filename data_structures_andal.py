f687d48410035c7b6233178eb7817dd0c7fbdddc
Felixandra27 <45374682+Felixandra27@users.noreply.github.com>
2018-12-11 11:00:10 +0800

    Add files via upload

---
+++ data_structures_andal.py
@@ -0,0 +1,28 @@
+#DATA STUCTURES
+print("Data_Structures")
+print("LISTS")
+sea_creatures = 'shark', 'cuttlefish', 'squid', 'mantis shrimp', 'anemone'
+print(sea_creatures)
+
+print("SETS")
+a = set([1,2,3,4])
+b = set([3,4,5,6])
+a | b #union
+{1,2,3,4,5,6}
+print(a)
+print(b)
+
+print("TUPLES")
+l = (1,2,3)
+l[0]
+(1,2)
+print(l)
+
+print("DICTIONARY")
+
+d = {'first':'string value', 'second':[1,2]}
+d.keys()
+['first', 'second']
+d.values()
+['string value', [1, 2]]
+print(d)
