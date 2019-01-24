2509a5c4739d57f3d07a2d9e525e34a8bd022988
Felixandra27 <45374682+Felixandra27@users.noreply.github.com>
2019-01-24 12:05:24 +0800

    Update fuction_andal.py

--- fuction_andal.py
+++ fuction_andal.py
@@ -1,3 +1,8 @@
 print("Name : Felixandra F. Andal")
 print("BS IT - 1A")
-print("ACTIVITY I.  Date: Jan. 22, 2019")
+print("ACTIVITY II.  Date: Jan. 22, 2019")
+
+def mul (a,b):
+  print(b,a)
+mul(a=39, b = 27)
+mul(a=30, b= 5.5)
