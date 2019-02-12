49992e57afcdaa279d11d5c9e1139a825cfcc8a3
Felixandra27 <felixandra.andal27@gmail.com>
2019-02-12 08:50:07 +0800

    Add files via upload

---
+++ list2D_andal.py
@@ -0,0 +1,17 @@
+#List of Two Dimentional Array
+
+print ("NAME : Felixandra Andal")
+print ("-----------------------")
+print (" ACTIVITY A ")
+print ("-----------------------")
+
+list2d = [[1,2,3,4], [5,6,7,8], [9,10,11,12]]
+
+for list in list2d[0]:
+    print(list)
+
+for list in list2d[1]:
+    print(list)
+
+for list in list2d[2]:
+    print(list)
